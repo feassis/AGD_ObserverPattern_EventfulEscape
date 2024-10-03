d8e6ef439572d16a00af32d92d8a53fd6edbbe17
Felipe Assis <felipe.assis.2002@gmail.com>
2024-10-03 16:38:23 -0300

    adding oberser on Playercontroler to toggle the sanity drain

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,6 +27,8 @@ public class PlayerController
         this.playerScriptableObject.KeysEquipped = 0;
 
         playerState = PlayerState.InDark;
+
+        LightSwitchView.lightSwitch += OnLightSwitchToggle;
     }
 
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
@@ -72,5 +74,13 @@ public class PlayerController
 
         rotation = playerRigidbody.rotation * Quaternion.Euler(lookRotation);
         position = (transform.position) + (velocity * movement) * Time.fixedDeltaTime;
+    }
+
+    private void OnLightSwitchToggle()
+    {
+        if(PlayerState == PlayerState.InDark)
+            PlayerState = PlayerState.None;
+        else
+            PlayerState = PlayerState.InDark;
     }
 }
